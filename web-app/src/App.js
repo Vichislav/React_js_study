da63c18917cb928fd7a63e47200a55cfb662ea24
Vichislav <s1ava8968@gmail.com>
2021-09-17 22:39:20 +0600

    nev part of web-app

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import About from './About';
+import Users from './Users';
+import Main from './Main';
+import UserId from './Userid';
+import Error from './Error';
+
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Header/>
+     <Router>
+       <Switch>
+         <Route exact path="/" component={Main}/> 
+         <Route path="/about" component={About}/>
+         <Route exact path="/users" component={Users}/>
+         <Route path="/users/:userName" component={UserId}/>
+         <Route component={Error} />
+       </Switch>
+     </Router>
+    </>
   );
 }
 
